69078c4225ce43d7ca07a30bfcd76e7c2806d400
fernandohspl <fernandohsp25@gmail.com>
2022-08-25 19:25:17 -0300

    aula25/8

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 Route::get('/hello-world', function () {
     return ('HELLO WORLD!!');
 });
+Route::get('/hello-world/{nome}', function ($nome) {
+    return ('HELLO'.$nome.'!!');
+});
