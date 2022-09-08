240f999098d39cfdd45224f0798dc0d271156bcd
fernandohspl <fernandohsp25@gmail.com>
2022-09-08 19:39:50 -0300

    aula8/09/2022

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -15,7 +15,8 @@ class CategoriaController extends Controller
     public function index()
     {
         //
-        return view('admin.main');
+        $categorias = Categoria::all();
+        return view('admin.categorias.index', compact('categorias'));
     }
 
     /**
@@ -26,6 +27,7 @@ class CategoriaController extends Controller
     public function create()
     {
         //
+        return view('admin.categorias.create');
     }
 
     /**
@@ -37,6 +39,8 @@ class CategoriaController extends Controller
     public function store(Request $request)
     {
         //
+        Categoria::create($request->all());
+        return redirect()->route('categorias.index');
     }
 
     /**
@@ -82,5 +86,98 @@ class CategoriaController extends Controller
     public function destroy(Categoria $categoria)
     {
         //
+        $categoria->delete();
+        return redirect()->route('categorias.index');
+    }
+}
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $categorias = Categoria::all();
+        return view('admin.categorias.index', compact('categorias'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('admin.categorias.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        Categoria::create($request->all());
+        return redirect()->route('categorias.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Categoria $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Categoria $categoria)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\Categoria $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Categoria $categoria)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Categoria $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Categoria $categoria)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Categoria $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Categoria $categoria)
+    {
+        //
+        $categoria->delete();
+        return redirect()->route('categorias.index');
     }
 }
