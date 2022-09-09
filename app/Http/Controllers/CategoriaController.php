d1cfd7e43c36fc96cd3629facab73cb03f713f78
fernandohspl <fernandohsp25@gmail.com>
2022-09-08 21:42:11 -0300

    aula8/09/2022

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -12,98 +12,13 @@ class CategoriaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Categoria $categoria)
     {
         //
         $categorias = Categoria::all();
-        return view('admin.categorias.index', compact('categorias'));
+        return view('admin.categorias.edit', compact('categoria'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-        return view('admin.categorias.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-        Categoria::create($request->all());
-        return redirect()->route('categorias.index');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Categoria  $categoria
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Categoria $categoria)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Categoria  $categoria
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Categoria $categoria)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Categoria  $categoria
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Categoria $categoria)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Categoria  $categoria
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Categoria $categoria)
-    {
-        //
-        $categoria->delete();
-        return redirect()->route('categorias.index');
-    }
-}
-
-namespace App\Http\Controllers;
-
-use App\Models\Categoria;
-use Illuminate\Http\Request;
-
-class CategoriaController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function edit()
     {
         //
         $categorias = Categoria::all();
@@ -165,7 +80,9 @@ class CategoriaController extends Controller
      */
     public function update(Request $request, Categoria $categoria)
     {
-        //
+        $params =$request->all();
+        $categoria->update($params);
+        return redirect()->route('categorias');
     }
 
     /**
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -20,7 +20,8 @@
                     <tr>
                         <td>{{$categoria->nome}}</td>
                         <td>
-                            <form action="{{route('categorias.destroy', $categoria)}}" method="post">
+                            <a heref="{{route('categorias.edit',$categoria)}}"class="btn btn-primary"Editar</a>
+                            <form action="{{route('categorias.destroy', $categoria)}}" method="post"> class"form-delete">
                                 @method('DELETE')
                                 @csrf
                                 <button class="btn btn-danger" type="submit">Excluir</button>
